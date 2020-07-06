9778897ce408162a226499bd436bce0b49f25b78
eugenetaranov-opsfleet <66247386+eugenetaranov-opsfleet@users.noreply.github.com>
2020-07-06 18:14:08 +0300

    Tools 30 remote state migration (#5)
    * created s3 bucket + dynamodb table
    
    * migrated tf-org tfstate to new s3 bucket
    
    * migrated tf-backup tfstate to new s3 bucket
    
    * removed old tstate s3 bucket

--- terraform/root-162540680133/remote-state/main.tf
+++ terraform/root-162540680133/remote-state/main.tf
@@ -3,36 +3,27 @@ provider "aws" {
   region  = var.aws_region
 }
 
-resource "aws_s3_bucket" "tfstate" {
-  bucket_prefix = "tfstate-"
-  acl           = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
+module "terraform_state_backend" {
+  source         = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=0.18.0"
+  region         = "us-east-1"
+  name           = "terraform"
+  s3_bucket_name = "tip-org-tfstate"
+  attributes     = ["state"]
 }
 
-resource "aws_s3_bucket_public_access_block" "tfstate" {
-  bucket                  = aws_s3_bucket.tfstate.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+output "remote_state_config" {
+  value = <<EOF
 
-output "tfstate_s3" {
-  value = aws_s3_bucket.tfstate.id
+terraform {
+  required_version = ">= 0.12.2"
+
+  backend "s3" {
+    region         = "${var.aws_region}"
+    bucket         = "${module.terraform_state_backend.s3_bucket_id}"
+    key            = "CHANGE ME!!"
+    dynamodb_table = "${module.terraform_state_backend.dynamodb_table_name}"
+    encrypt        = true
+  }
+}
+EOF
 }
