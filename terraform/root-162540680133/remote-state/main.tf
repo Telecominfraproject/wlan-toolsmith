82f01a8c752548f29b5a5738ed20b1e2abbe62dd
Eugene Taranov <eugene@opsfleet.com>
2020-07-06 14:47:01 +0300

    removed old tstate s3 bucket

--- terraform/root-162540680133/remote-state/main.tf
+++ terraform/root-162540680133/remote-state/main.tf
@@ -3,40 +3,6 @@ provider "aws" {
   region  = var.aws_region
 }
 
-resource "aws_s3_bucket" "tfstate" {
-  bucket_prefix = "tfstate-"
-  acl           = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-}
-
-resource "aws_s3_bucket_public_access_block" "tfstate" {
-  bucket                  = aws_s3_bucket.tfstate.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-output "tfstate_s3" {
-  value = aws_s3_bucket.tfstate.id
-}
-
 module "terraform_state_backend" {
   source         = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=0.18.0"
   region         = "us-east-1"
