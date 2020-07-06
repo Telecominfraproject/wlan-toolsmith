e48b0c503d94ca4bc9ad7281d214f6fb6ecf55af
Eugene Taranov <eugene@opsfleet.com>
2020-07-06 14:33:08 +0300

    created s3 bucket + dynamodb table

--- terraform/root-162540680133/remote-state/main.tf
+++ terraform/root-162540680133/remote-state/main.tf
@@ -36,3 +36,28 @@ resource "aws_s3_bucket_public_access_block" "tfstate" {
 output "tfstate_s3" {
   value = aws_s3_bucket.tfstate.id
 }
+
+module "terraform_state_backend" {
+  source         = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=0.18.0"
+  region         = "us-east-1"
+  name           = "terraform"
+  s3_bucket_name = "tip-org-tfstate"
+  attributes     = ["state"]
+}
+
+output "remote_state_config" {
+  value = <<EOF
+
+terraform {
+  required_version = ">= 0.12.2"
+
+  backend "s3" {
+    region         = "${var.aws_region}"
+    bucket         = "${module.terraform_state_backend.s3_bucket_id}"
+    key            = "CHANGE ME!!"
+    dynamodb_table = "${module.terraform_state_backend.dynamodb_table_name}"
+    encrypt        = true
+  }
+}
+EOF
+}
