23a1cfada154910ad3e58ac08afccfe01a325189
eugenetaranov-opsfleet <66247386+eugenetaranov-opsfleet@users.noreply.github.com>
2020-07-14 16:59:24 +0300

    TOOLS-39: remote state resources for terraform for WIFI account (#8)
    * TOOLS-39: remote state resources for terraform for WIFI account
    
    * fixed dir structure
    
    Co-authored-by: Eugene Taranov <eugene@taranov.me>

---
+++ terraform/wifi-289708231103/remote-state/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  version = ">= 2.63.0"
+  region  = var.aws_region
+}
+
+module "terraform_state_backend" {
+  source         = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=0.18.0"
+  region         = "us-east-1"
+  name           = "terraform"
+  s3_bucket_name = "tip-wifi-tfstate"
+  attributes     = ["state"]
+}
+
+output "remote_state_config" {
+  value = <<EOF
+
+terraform {
+  required_version = ">= 0.12.2"
+
+  backend "s3" {
+    region         = "${var.aws_region}"
+    bucket         = "${module.terraform_state_backend.s3_bucket_id}"
+    key            = "CHANGE ME!!"
+    dynamodb_table = "${module.terraform_state_backend.dynamodb_table_name}"
+    encrypt        = true
+  }
+}
+EOF
+}
