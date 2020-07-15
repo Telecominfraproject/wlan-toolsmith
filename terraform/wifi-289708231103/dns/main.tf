9d979cce74306e4a40dab97433019035b3a4249d
eugenetaranov-opsfleet <66247386+eugenetaranov-opsfleet@users.noreply.github.com>
2020-07-15 22:30:29 +0300

    TOOLS-39: lab.wlan.tip.build dns zone (#9)
    * TOOLS-39: remote state resources for terraform for WIFI account
    
    * fixed dir structure
    
    * TOOLS-39: lab.wlan.tip.build zone
    
    Co-authored-by: Eugene Taranov <eugene@taranov.me>

---
+++ terraform/wifi-289708231103/dns/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  version = ">= 2.59.0"
+  region  = var.aws_region
+}
+
+terraform {
+  required_version = ">= 0.12.2"
+
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "tip-wifi-tfstate"
+    key            = "dns"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+resource "aws_route53_zone" "main" {
+  name = var.main_zone_name
+}
---
+++ terraform/wifi-289708231103/dns/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS zone"
+  type        = string
+}
+
+variable "main_zone_name" {
+  description = "Main zone name"
+  type        = string
+}
