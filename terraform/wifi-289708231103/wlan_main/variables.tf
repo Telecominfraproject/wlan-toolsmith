4c48985e2af13c6b2896f7ea196895b1d4810c30
eugenetaranov-opsfleet <66247386+eugenetaranov-opsfleet@users.noreply.github.com>
2020-07-23 13:59:04 +0300

    add missing key (#16)
    Co-authored-by: Eugene Taranov <eugene@taranov.me>

--- terraform/wifi-289708231103/wlan_main/variables.tf
+++ terraform/wifi-289708231103/wlan_main/variables.tf
@@ -30,3 +30,9 @@ variable "root_org_account" {
   type        = string
   default     = "162540680133"
 }
+
+variable "ssh_key" {
+  description = "Default public ssh key"
+  type = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwLEqyg/ob9w7QNLJI2ldpFzQjvjD0Fz/h5Y2Yrl6ijU7Cs4dOCZJonO7L42luV8tHI1kk4rSpEMbNou6HGWZUYvCMB9dyABYEOtc3a0y+psPw/xNHxDIkDzQ868bktFG2DcJqN2Si6t8pjnLikyBVNCmRNQp2JCa71vL1m1LsnQ5BHYzccXlzzpL0C7yWBUSLlv2l83OMveS8ltZPzXkKo5HkbGBBHGSURAyfLmni6Hz//YQX5yMY+ECrzcKxhki17MYm9OU1rVa5b/Yyntl8jYq6ldx2srik9/OfZzB2PY8w/LF915cg0OqWDkCswwStx7Gr2HjXIQLGnFKWS/V342VKKj+ccpCL/QGGOPzquRglTjFlfD5dssAudQPr1x49kaBE9wg9iFspmozT1sMOUt7zqOfpSIxuEv4zTViE63ClP7SDhgFgXeUj4Trje8562YB5D0ChpxbyIhIQd4rm7zEZoq6hR6/HxvdrKKjxYiqsVwgxCrFiH5NoozOUvqM="
+}
