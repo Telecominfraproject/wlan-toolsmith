f4ee28dbebe14694a298ca0f350743b44f3f6fb6
eugenetaranov-opsfleet <66247386+eugenetaranov-opsfleet@users.noreply.github.com>
2020-07-23 14:29:17 +0300

    recreate instances in 10.10.1.0/24 (#17)
    Co-authored-by: Eugene Taranov <eugene@taranov.me>

--- terraform/wifi-289708231103/wlan_main/instance_tws_1.tf
+++ terraform/wifi-289708231103/wlan_main/instance_tws_1.tf
@@ -21,7 +21,7 @@ data "aws_ami" "wlan_tws_1" {
 resource "aws_instance" "wlan_tws_1" {
   ami                    = data.aws_ami.wlan_tws_1.id
   instance_type          = "t3.micro"
-  subnet_id              = module.vpc_main.public_subnets[0]
+  subnet_id              = module.vpc_main.public_subnets[1]
   vpc_security_group_ids = [aws_security_group.wlan.id]
   key_name               = aws_key_pair.wlan.id
 
