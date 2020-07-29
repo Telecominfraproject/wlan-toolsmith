95ae68007961ae7cbeff478d59f755cd48448e42
eugenetaranov-opsfleet <66247386+eugenetaranov-opsfleet@users.noreply.github.com>
2020-07-29 23:13:13 +0300

    WIFI-328: eks module (#14)
    * WIFI-328: EKS cluster for wifi project
    
    * EKS tf module
    
    Co-authored-by: Eugene Taranov <eugene@taranov.me>

---
+++ tf_modules/eks/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  version = ">= 2.59.0"
+  region  = var.aws_region
+}
+
+terraform {
+  required_version = ">= 0.12.2"
+
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "tip-wifi-tfstate"
+    key            = "wlan-main"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+resource "aws_key_pair" "wlan" {
+  key_name   = "wlan"
+  public_key = file("id_rsa.pub")
+}
