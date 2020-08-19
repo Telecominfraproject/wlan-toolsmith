2edf40655519e35bb3d342881aaf947ed40c5a5f
Eugene Taranov <eugene@taranov.me>
2020-08-19 17:22:59 +0300

    added amazon-ssm-agent

--- terraform/wifi-289708231103/wlan_main/eks.tf
+++ terraform/wifi-289708231103/wlan_main/eks.tf
@@ -37,7 +37,12 @@ module "eks" {
       k8s_labels = {
         role = "default"
       }
-      additional_tags = local.tags
+      additional_tags     = local.tags
+      additional_userdata = <<EOF
+yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+systemctl enable amazon-ssm-agent
+systemctl start amazon-ssm-agent
+EOF
     }
   ]
 
