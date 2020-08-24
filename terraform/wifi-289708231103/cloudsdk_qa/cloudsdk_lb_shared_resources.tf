eb7a03ede51c2be75a6613fa840b60ec10012fee
Eugene Taranov <eugene@taranov.me>
2020-08-24 16:18:03 +0300

    added QA env

---
+++ terraform/wifi-289708231103/cloudsdk_qa/cloudsdk_lb_shared_resources.tf
@@ -0,0 +1,133 @@
+resource "aws_security_group" "cloudsdk_lb" {
+  name        = "cloudsdk-${var.deployment}-lb"
+  description = "SG for EKS LBs servicing ${local.cluster_name}/${var.deployment}} EKS cluster"
+  vpc_id      = module.vpc_main.vpc_id
+  tags        = local.tags
+}
+
+resource "aws_security_group_rule" "cloudsdk_lb_egress" {
+  from_port         = 0
+  to_port           = 65535
+  protocol          = -1
+  security_group_id = aws_security_group.cloudsdk_lb.id
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "cloudsdk_lb_ingress_http" {
+  for_each          = toset(["80", "443"])
+  from_port         = each.key
+  to_port           = each.key
+  protocol          = "TCP"
+  security_group_id = aws_security_group.cloudsdk_lb.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_s3_bucket" "alb_logs" {
+  bucket_prefix = "alb-logs-"
+  acl           = "private"
+
+  versioning {
+    enabled = false
+  }
+
+  lifecycle_rule {
+    prefix  = ""
+    enabled = true
+
+    transition {
+      days          = 30
+      storage_class = "ONEZONE_IA"
+    }
+
+    expiration {
+      days = 60
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = local.tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "alb_logs" {
+  bucket                  = aws_s3_bucket.alb_logs.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "alb_logs_policy" {
+  bucket = aws_s3_bucket.alb_logs.id
+  policy = data.aws_iam_policy_document.alb_logs_policy.json
+}
+
+data "aws_iam_policy_document" "alb_logs_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = ["${aws_s3_bucket.alb_logs.arn}/*"]
+
+    // Elastic Load Balancing Account ID in us-east-2
+    // https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::033677994240:root",
+      ]
+    }
+  }
+}
+
+resource "aws_acm_certificate" "cloudsdk" {
+  domain_name = format("%s.%s", var.deployment, var.base_domain)
+  subject_alternative_names = [
+    format("*.%s.%s", var.deployment, var.base_domain)
+  ]
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cloudsdk_ssl_validation" {
+  zone_id = aws_route53_zone.cloudsdk.id
+  name    = aws_acm_certificate.cloudsdk.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.cloudsdk.domain_validation_options.0.resource_record_type
+  ttl     = 600
+  records = [
+    aws_acm_certificate.cloudsdk.domain_validation_options.0.resource_record_value
+  ]
+}
+
+resource "aws_route53_zone" "cloudsdk" {
+  name = format("%s.%s", var.deployment, var.base_domain)
+}
+
+resource "aws_route53_record" "aws_route53_zone_cloudsdk_main_glue" {
+  allow_overwrite = true
+  name            = format("%s.%s", var.deployment, var.base_domain)
+  ttl             = 60
+  type            = "NS"
+  zone_id         = data.terraform_remote_state.route_53.outputs.zone_id
+  records         = aws_route53_zone.cloudsdk.name_servers
+}
---
+++ terraform/wifi-289708231103/cloudsdk_qa/variables.tf
@@ -0,0 +1,81 @@
+variable "aws_region" {
+  description = "AWS zone"
+  type        = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "az" {
+  default = ["a", "b", "c"]
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+}
+
+variable "org" {
+  description = "Organization name"
+  type        = string
+}
+
+variable "root_org_account" {
+  description = "Root org account id"
+  type        = string
+  default     = "162540680133"
+}
+
+variable "node_group_settings" {
+  description = "Cluster node group settings"
+  type        = map(string)
+  default = {
+    max_capacity  = 1
+    min_capacity  = 1
+    instance_type = "t3.small"
+    disk_size     = 20
+  }
+}
+
+variable "cluster_log_retention_in_days" {
+  description = "Cloudwatch logs retention (days)"
+  type        = number
+  default     = 30
+}
+
+variable "cluster_version" {
+  description = "EKS cluster version"
+  type        = string
+}
+
+variable "eks_admin_roles" {
+  description = "List of role names with system:masters permissions on EKS"
+  type        = set(string)
+  default     = []
+}
+
+variable "base_domain" {
+  description = "Public domain name"
+  type        = string
+}
+
+variable "deployment" {
+  description = "Deployment name"
+  type        = string
+}
+
+variable "service_ingress" {
+  description = "Load balancer configuration for ELK services"
+  type = map(object({
+    internal_protocol = string
+    internal_port     = number
+    external_protocol = string
+    external_port     = number
+    healthcheck_path  = string
+  }))
+}
