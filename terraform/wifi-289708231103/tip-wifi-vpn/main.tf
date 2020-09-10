955aba636a74477247b4dc52324e3e899bc78792
Eugene Taranov <eugene@taranov.me>
2020-09-10 16:32:38 +0300

    WIFI-496: added transitgway, attached to cicd VPC and setup VPN site2site

---
+++ terraform/wifi-289708231103/tip-wifi-vpn/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = var.region
+}
+
+terraform {
+  required_version = ">= 0.12.2"
+
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "tip-wifi-tfstate"
+    key            = "wlan-vpn"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+data "terraform_remote_state" "wlan_main" {
+  backend = "s3"
+
+  config = {
+    region         = "us-east-1"
+    bucket         = "tip-wifi-tfstate"
+    key            = "wlan-main"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
