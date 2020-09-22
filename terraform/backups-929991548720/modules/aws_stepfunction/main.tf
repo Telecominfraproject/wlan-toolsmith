b206536defcd072aeb137739dd6a0cfa1eb478dc
Eugene Taranov <eugene@taranov.me>
2020-09-22 17:55:19 +0300

    TOOLS-58: backups

---
+++ terraform/backups-929991548720/modules/aws_stepfunction/main.tf
@@ -0,0 +1,133 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on      = [aws_iam_role.sfn_role]
+  create_duration = "30s"
+}
+
+resource "aws_sfn_state_machine" "main" {
+  name       = var.name
+  role_arn   = aws_iam_role.sfn_role.arn
+  definition = var.step_function_definition
+  tags       = var.tags
+  depends_on = [time_sleep.wait_30_seconds]
+}
+
+resource "aws_iam_role" "sfn_role" {
+  name               = "${var.name}-sfn"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.sfn_role_assume_policy.json
+  tags               = var.tags
+}
+
+data "aws_iam_policy_document" "sfn_role_assume_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["states.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "sfn_role" {
+  name   = var.name
+  role   = aws_iam_role.sfn_role.id
+  policy = data.aws_iam_policy_document.sfn_repo_backup_policy.json
+}
+
+data "aws_iam_policy_document" "sfn_repo_backup_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:RunTask",
+    ]
+    resources = [var.ecs_task_definition]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:StopTask",
+      "ecs:DescribeTasks"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:PassRole",
+    ]
+    resources = [
+      var.ecs_task_role,
+      var.ecs_task_execution_role
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "events:PutTargets",
+      "events:PutRule",
+      "events:DescribeRule"
+    ]
+    resources = [
+      "arn:aws:events:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:rule/StepFunctionsGetEventsForECSTaskRule"
+    ]
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "main" {
+  name                = var.name
+  schedule_expression = var.cron_schedule
+}
+
+resource "aws_cloudwatch_event_target" "repo_backup" {
+  rule     = aws_cloudwatch_event_rule.main.id
+  arn      = aws_sfn_state_machine.main.id
+  role_arn = aws_iam_role.cw_trigger.arn
+}
+
+resource "aws_iam_role" "cw_trigger" {
+  name               = "${var.name}-cw-trigger"
+  assume_role_policy = data.aws_iam_policy_document.cw_trigger_assume_policy.json
+  tags               = var.tags
+}
+
+data "aws_iam_policy_document" "cw_trigger_assume_policy" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "events.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "cw_trigger_policy" {
+  name   = var.name
+  role   = aws_iam_role.cw_trigger.id
+  policy = data.aws_iam_policy_document.cw_trigger_policy.json
+}
+
+data "aws_iam_policy_document" "cw_trigger_policy" {
+  statement {
+    sid    = "RunStepFunction"
+    effect = "Allow"
+    actions = [
+      "states:StartExecution"
+    ]
+    resources = [
+      aws_sfn_state_machine.main.id
+    ]
+  }
+}
---
+++ terraform/backups-929991548720/tf_backup/s3.tf
@@ -0,0 +1,59 @@
+resource "aws_s3_bucket" "repo_backup" {
+  bucket = var.s3_bucket_backup_name
+  acl    = "private"
+
+  versioning {
+    enabled = var.s3_bucket_versioning
+  }
+
+  lifecycle_rule {
+    prefix  = ""
+    enabled = true
+
+    transition {
+      days          = 7
+      storage_class = "INTELLIGENT_TIERING"
+    }
+
+    noncurrent_version_transition {
+      days          = 7
+      storage_class = "INTELLIGENT_TIERING"
+    }
+
+    noncurrent_version_expiration {
+      days = var.backup_retention_period
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "repo_backup" {
+  bucket                  = aws_s3_bucket.repo_backup.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id          = data.aws_vpc.default.id
+  service_name    = "com.amazonaws.${var.aws_region}.s3"
+  auto_accept     = true
+  route_table_ids = data.aws_route_tables.default.ids
+}
+
+data "aws_route_tables" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
