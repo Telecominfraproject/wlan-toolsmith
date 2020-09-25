7ab01d8c0b6e8dab59e8aa607b4677eadd7f01dc
eugenetaranov-opsfleet <66247386+eugenetaranov-opsfleet@users.noreply.github.com>
2020-09-25 12:11:36 +0300

    TOOLS-58: moving backup infra into dedicated AWS account (#41)
    * TOOLS-58: backups
    
    * upd readme
    
    * fix missing cloudwatch logs
    
    Co-authored-by: Eugene Taranov <eugene@taranov.me>

---
+++ terraform/backups-929991548720/tf_backup/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 0.13.3"
+
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "tip-backups-tfstate"
+    key            = "tip-backup"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  version = ">= 2.63.0"
+  region  = var.aws_region
+}
+
+data "aws_vpc" "default" {
+  filter {
+    name   = "isDefault"
+    values = ["true"]
+  }
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_caller_identity" "current" {}
