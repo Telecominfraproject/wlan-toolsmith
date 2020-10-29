df34a47de44218b9aa9bb56385ed45c4862086d1
Eugene Taranov <eugene@opsfleet.com>
2020-10-29 16:39:48 +0300

    WIFI-496: synced terraform code with actual state

--- terraform/wifi-289708231103/tip-wifi-vpn/vars.tf
+++ terraform/wifi-289708231103/tip-wifi-vpn/vars.tf
@@ -7,4 +7,10 @@ variable "vpn_endpoint_ip" {
 
 variable "vpn_endpoint_cidr" {
   description = "Subnet behind the VPN endpoint $vpn_endpoint_ip"
+  type        = string
+}
+
+variable "nrg_vpn_endpoint_ip" {
+  description = "IP address of the VPN endpoint connecting to AWS"
+  type        = string
 }
--- terraform/wifi-289708231103/tip-wifi-vpn/vpn.tf
+++ terraform/wifi-289708231103/tip-wifi-vpn/vpn.tf
@@ -1,3 +1,4 @@
+// to be deleted
 resource "aws_customer_gateway" "tip_wifi_nrg" {
   bgp_asn    = 65000
   ip_address = var.vpn_endpoint_ip
@@ -8,6 +9,7 @@ resource "aws_customer_gateway" "tip_wifi_nrg" {
   }
 }
 
+// to be deleted
 resource "aws_vpn_connection" "tip_wifi_nrg" {
   customer_gateway_id = aws_customer_gateway.tip_wifi_nrg.id
   transit_gateway_id  = module.tgw_main.this_ec2_transit_gateway_id
@@ -15,5 +17,25 @@ resource "aws_vpn_connection" "tip_wifi_nrg" {
   static_routes_only  = true
   tags = {
     Name = "tip-wifi-nrg"
+  }
+}
+
+resource "aws_customer_gateway" "tunnel_tip_wifi_nrg" {
+  bgp_asn    = 65000
+  ip_address = var.nrg_vpn_endpoint_ip
+  type       = "ipsec.1"
+
+  tags = {
+    Name = "tip-wifi-nrg"
+  }
+}
+
+resource "aws_vpn_connection" "tunnel_tip_wifi_nrg" {
+  customer_gateway_id = aws_customer_gateway.tunnel_tip_wifi_nrg.id
+  transit_gateway_id  = module.tgw_main.this_ec2_transit_gateway_id
+  type                = "ipsec.1"
+  static_routes_only  = true
+  tags = {
+    Name = "tunnel-tip-wifi-nrg"
   }
 }
