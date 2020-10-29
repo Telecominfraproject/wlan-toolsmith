f11ee59141e051ca0788fd3c54f78b464df62524
eugenetaranov-opsfleet <66247386+eugenetaranov-opsfleet@users.noreply.github.com>
2020-10-29 16:57:09 +0300

    WIFI-496: synced terraform code with actual state (#45)

--- terraform/wifi-289708231103/tip-wifi-vpn/transit_gateway.tf
+++ terraform/wifi-289708231103/tip-wifi-vpn/transit_gateway.tf
@@ -27,6 +27,6 @@ resource "aws_route" "private" {
 
 resource "aws_ec2_transit_gateway_route" "vpn" {
   destination_cidr_block         = var.vpn_endpoint_cidr
-  transit_gateway_attachment_id  = aws_vpn_connection.tip_wifi_nrg.transit_gateway_attachment_id
+  transit_gateway_attachment_id  = aws_vpn_connection.tunnel_tip_wifi_nrg.transit_gateway_attachment_id
   transit_gateway_route_table_id = module.tgw_main.this_ec2_transit_gateway_association_default_route_table_id
 }
