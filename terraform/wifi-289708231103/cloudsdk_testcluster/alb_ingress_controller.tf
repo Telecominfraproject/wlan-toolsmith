73469cd139dbea09fcd91ccc4334e09284da7483
Eugene Taranov <eugene@opsfleet.com>
2020-11-13 12:40:00 +0300

    fix IAM permissions for alb ingress

--- terraform/wifi-289708231103/cloudsdk_testcluster/alb_ingress_controller.tf
+++ terraform/wifi-289708231103/cloudsdk_testcluster/alb_ingress_controller.tf
@@ -1,9 +1,11 @@
 module "alb_ingress_iam_role" {
-  source           = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-assumable-role-with-oidc?ref=v2.12.0"
-  role_name        = "${module.eks.cluster_id}-alb-ingress"
-  provider_url     = local.oidc_provider_url
-  role_policy_arns = [aws_iam_policy.external_dns.arn]
-  create_role      = true
+  source       = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-assumable-role-with-oidc?ref=v2.12.0"
+  role_name    = "${module.eks.cluster_id}-alb-ingress"
+  provider_url = local.oidc_provider_url
+  role_policy_arns = [
+    aws_iam_policy.alb_ingress_iam_policy.arn,
+  ]
+  create_role = true
 }
 
 resource "aws_iam_policy" "alb_ingress_iam_policy" {
