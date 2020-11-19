99b343f036bcf2ebcd22c1f05319d07dd7d5aaca
eugenetaranov-opsfleet <66247386+eugenetaranov-opsfleet@users.noreply.github.com>
2020-11-19 18:36:11 +0300

    WIFI-496: cleanup test vpn channel (#47)
    Co-authored-by: Eugene Taranov <eugene@taranov.me>

--- terraform/wifi-289708231103/tip-wifi-vpn/vpn.tf
+++ terraform/wifi-289708231103/tip-wifi-vpn/vpn.tf
@@ -1,25 +1,3 @@
-// to be deleted
-resource "aws_customer_gateway" "tip_wifi_nrg" {
-  bgp_asn    = 65000
-  ip_address = var.vpn_endpoint_ip
-  type       = "ipsec.1"
-
-  tags = {
-    Name = "tip-wifi-nrg"
-  }
-}
-
-// to be deleted
-resource "aws_vpn_connection" "tip_wifi_nrg" {
-  customer_gateway_id = aws_customer_gateway.tip_wifi_nrg.id
-  transit_gateway_id  = module.tgw_main.this_ec2_transit_gateway_id
-  type                = "ipsec.1"
-  static_routes_only  = true
-  tags = {
-    Name = "tip-wifi-nrg"
-  }
-}
-
 resource "aws_customer_gateway" "tunnel_tip_wifi_nrg" {
   bgp_asn    = 65000
   ip_address = var.nrg_vpn_endpoint_ip
