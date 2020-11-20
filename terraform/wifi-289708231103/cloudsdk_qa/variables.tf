796e19e09cf17ab856fe6e1d293671f90cac3f12
Eugene Taranov <eugene@opsfleet.com>
2020-11-20 11:42:03 +0300

    WIFI-991: alb-ingress for qa cluster

--- terraform/wifi-289708231103/cloudsdk_qa/variables.tf
+++ terraform/wifi-289708231103/cloudsdk_qa/variables.tf
@@ -31,6 +31,12 @@ variable "root_org_account" {
   default     = "162540680133"
 }
 
+variable "ssh_key" {
+  description = "Default public ssh key"
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwLEqyg/ob9w7QNLJI2ldpFzQjvjD0Fz/h5Y2Yrl6ijU7Cs4dOCZJonO7L42luV8tHI1kk4rSpEMbNou6HGWZUYvCMB9dyABYEOtc3a0y+psPw/xNHxDIkDzQ868bktFG2DcJqN2Si6t8pjnLikyBVNCmRNQp2JCa71vL1m1LsnQ5BHYzccXlzzpL0C7yWBUSLlv2l83OMveS8ltZPzXkKo5HkbGBBHGSURAyfLmni6Hz//YQX5yMY+ECrzcKxhki17MYm9OU1rVa5b/Yyntl8jYq6ldx2srik9/OfZzB2PY8w/LF915cg0OqWDkCswwStx7Gr2HjXIQLGnFKWS/V342VKKj+ccpCL/QGGOPzquRglTjFlfD5dssAudQPr1x49kaBE9wg9iFspmozT1sMOUt7zqOfpSIxuEv4zTViE63ClP7SDhgFgXeUj4Trje8562YB5D0ChpxbyIhIQd4rm7zEZoq6hR6/HxvdrKKjxYiqsVwgxCrFiH5NoozOUvqM="
+}
+
 variable "node_group_settings" {
   description = "Cluster node group settings"
   type        = map(string)
@@ -68,15 +74,4 @@ variable "base_domain" {
 variable "deployment" {
   description = "Deployment name"
   type        = string
-}
-
-variable "service_ingress" {
-  description = "Load balancer configuration for ELK services"
-  type = map(object({
-    internal_protocol = string
-    internal_port     = number
-    external_protocol = string
-    external_port     = number
-    healthcheck_path  = string
-  }))
 }
