389e9e83e44f573e43cd31213bb1dce85365f27c
Max <xamrennerb@gmail.com>
2021-01-22 12:54:32 +0100

    add Atlantis deployment (#77)
    * add Atlantis configuration file
    * add Atlantis deployment
    * set default Terraform version for Atlantis
    * set apply requirements for Atlantis
    * add atlantis folder to managed projects

---
+++ terraform/wifi-289708231103/atlantis/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "tip-wifi-tfstate"
+    key            = "atlantis"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "terraform_remote_state" "route_53" {
+  backend = "s3"
+
+  config = {
+    region         = "us-east-1"
+    bucket         = "tip-wifi-tfstate"
+    key            = "dns"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+module "atlantis" {
+  source  = "terraform-aws-modules/atlantis/aws"
+  version = "~> 2.0"
+
+  name = "atlantis"
+
+  cidr            = "10.20.0.0/16"
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  private_subnets = ["10.20.1.0/24", "10.20.2.0/24"]
+  public_subnets  = ["10.20.101.0/24", "10.20.102.0/24"]
+
+  route53_zone_name = trimsuffix(data.terraform_remote_state.route_53.outputs.zone_name, ".")
+
+  atlantis_github_user       = var.atlantis_github_user
+  atlantis_github_user_token = var.atlantis_github_user_token
+  atlantis_repo_whitelist    = var.repo_whitelist
+
+  policies_arn = var.atlantis_policy_arns
+
+  ecs_fargate_spot = true
+
+  tags = {
+    "ManagedBy" = "terraform"
+  }
+
+  custom_environment_variables = [
+    {
+      name  = "ATLANTIS_DEFAULT_TF_VERSION"
+      value = var.default_terraform_version
+    },
+    {
+      name  = "ATLANTIS_REPO_CONFIG_JSON"
+      value = file("atlantis.json")
+    }
+  ]
+
+}
+
+module "github_repository_webhook" {
+  source = "terraform-aws-modules/atlantis/aws//modules/github-repository-webhook"
+
+  github_organization = var.atlantis_github_organization
+  github_token        = var.atlantis_github_user_token
+
+  atlantis_allowed_repo_names = var.repo_names
+
+  webhook_url    = module.atlantis.atlantis_url_events
+  webhook_secret = module.atlantis.webhook_secret
+}
---
+++ terraform/wifi-289708231103/atlantis/terraform.tfvars
@@ -0,0 +1,8 @@
+aws_region                   = "eu-central-1"
+atlantis_github_user         = "tip-automation"
+atlantis_github_organization = "Telecominfraproject"
+atlantis_github_user_token   = "a004755f9848abcd9d7eee490f5f8c34114c5de9"
+atlantis_policy_arns         = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+repo_whitelist               = ["github.com/Telecominfraproject/Toolsmith"]
+repo_names                   = ["Toolsmith"]
+default_terraform_version    = "v0.13.5"
