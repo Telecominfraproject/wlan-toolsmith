cd9ee16de60d172e813bde501739e3e35a886788
Gleb Boushev <core@4c74356b41.com>
2021-02-10 16:39:05 +0300

    Feature/wifi 1350 (#86)
    * testbed deployments file
    
    * chmod aws-cicd-testbed-deployment.yaml.sh
    
    * dns name has to be lowercase
    
    * attempting to use different ports
    
    * removing fixed ports
    
    * nodeportstatic false for all services
    
    * Update aws-cicd-testbed-deployment.yaml.sh
    
    Co-authored-by: Gleb Boushev <4c74356b41@outlook.com>

---
+++ helm-values/aws-cicd-testbed-deployment.yaml.sh
@@ -0,0 +1,178 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$1" ];
+then
+  echo "testbed number has not been set";
+  exit 1
+fi
+
+TESTBED_NUMBER=$1
+TODAY=$(date +"%Y-%m-%d")
+
+cat <<EOF
+shared:
+  service:
+    srv-https-annotations: &srv-https-annotations
+      kubernetes.io/ingress.class: alb
+      alb.ingress.kubernetes.io/scheme: internet-facing
+      alb.ingress.kubernetes.io/group.name: wlan-cicd-nola-$TESTBED_NUMBER
+      alb.ingress.kubernetes.io/certificate-arn: arn:aws:acm:us-east-2:289708231103:certificate/bfa89c7a-5b64-4a8a-bcfe-ffec655b5285
+      alb.ingress.kubernetes.io/listen-ports: '[{"HTTP": 80}, {"HTTPS": 443}]'
+      alb.ingress.kubernetes.io/actions.ssl-redirect: '{"Type": "redirect", "RedirectConfig": { "Protocol": "HTTPS", "Port": "443", "StatusCode": "HTTP_302"}}'
+
+opensync-gw-static:
+  enabled: false
+
+common:
+  efs-provisioner:
+    enabled: false
+
+opensync-gw-cloud:
+  enabled: true
+  image:
+    tag: 0.0.1-SNAPSHOT-$TODAY
+  service:
+    type: LoadBalancer
+    nodePortStatic: false
+    annotations:
+      external-dns.alpha.kubernetes.io/hostname: wlan-filestore-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build,opensync-controller-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build,opensync-redirector-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+  externalhost:
+    address:
+      ovsdb: opensync-controller-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+      mqtt: opensync-mqtt-broker-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+  persistence:
+    enabled: false
+  filestore:
+    url: https://wlan-filestore-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+
+opensync-mqtt-broker:
+  enabled: true
+  service:
+    type: LoadBalancer
+    nodePortStatic: false
+    annotations:
+      external-dns.alpha.kubernetes.io/hostname: opensync-mqtt-broker-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+  persistence:
+    enabled: true
+    storageClass: gp2
+
+wlan-cloud-graphql-gw:
+  enabled: true
+  image:
+    tag: latest-$TODAY
+  service:
+    nodePortStatic: false
+  ingress:
+    annotations:
+      <<: *srv-https-annotations
+    enabled: true
+    alb_https_redirect: true
+    hosts:
+    - host: wlan-graphql-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+      paths: [
+        /*
+        ]
+  env:
+    portalsvc: wlan-portal-svc-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+
+wlan-cloud-static-portal:
+  enabled: true
+  image:
+    tag: latest-$TODAY
+  env:
+    graphql: https://wlan-graphql-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+  service:
+    type: NodePort
+  ingress:
+    annotations:
+      <<: *srv-https-annotations
+    alb_https_redirect: true
+    hosts:
+      - host: wlan-ui-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+        paths: [
+           /*
+          ]
+
+wlan-portal-service:
+  enabled: true
+  image:
+    tag: 0.0.1-SNAPSHOT-$TODAY
+  service:
+    type: NodePort
+    nodePortStatic: false
+  persistence:
+    enabled: true
+    storageClass: gp2
+    accessMode: ReadWriteOnce
+    filestoreSize: 10Gi
+  tsp:
+    host: wlan-portal-svc-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+  ingress:
+    enabled: true
+    alb_https_redirect: true
+    tls: []
+    annotations:
+      <<: *srv-https-annotations
+      alb.ingress.kubernetes.io/backend-protocol: HTTPS
+      alb.ingress.kubernetes.io/healthcheck-protocol: HTTPS
+      alb.ingress.kubernetes.io/healthcheck-port: traffic-port
+      alb.ingress.kubernetes.io/healthcheck-path: /ping
+    hosts:
+      - host: wlan-portal-svc-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+        paths: [
+           /*
+          ]
+
+wlan-prov-service:
+  enabled: true
+  image:
+    tag: 0.0.1-SNAPSHOT-$TODAY
+  service:
+    nodePortStatic: false
+
+wlan-ssc-service:
+  enabled: true
+  image:
+    tag: 0.0.1-SNAPSHOT-$TODAY
+  service:
+    nodePortStatic: false
+
+wlan-spc-service:
+  enabled: true
+  image:
+    tag: 0.0.1-SNAPSHOT-$TODAY
+  service:
+    nodePortStatic: false
+
+wlan-port-forwarding-gateway-service:
+  enabled: true
+  image:
+    tag: 0.0.1-SNAPSHOT-$TODAY
+  service:
+    nodePortStatic: false
+  creds:
+    websocketSessionTokenEncKey: MyToKeN0MyToKeN1
+  externallyVisible:
+    host: api.wlan-nola-$TESTBED_NUMBER.cicd.lab.wlan.tip.build
+    port: 30501
+  accessPointDebugPortRange:
+    length: 0
+
+kafka:
+  enabled: true
+  persistence:
+    storageClass: gp2
+
+cassandra:
+  enabled: true
+  persistence:
+    storageClass: gp2
+
+postgresql:
+  enabled: true
+  persistence:
+    storageClass: gp2
+
+EOF
