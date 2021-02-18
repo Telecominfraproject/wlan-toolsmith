1e04944331ee8bd862059332370c037c111e4274
Max <xamrennerb@gmail.com>
2021-02-18 12:55:58 +0100

    add EKS user for Quali poc (#93)

--- terraform/wifi-289708231103/cloudsdk_cicd/users.tf
+++ terraform/wifi-289708231103/cloudsdk_cicd/users.tf
@@ -20,3 +20,26 @@ data "aws_iam_policy_document" "gh-actions-policy" {
     resources = ["*"]
   }
 }
+
+resource "aws_iam_user" "quali-poc" {
+  name = "quali-poc"
+  path = "/"
+  tags = local.common_tags
+}
+
+resource "aws_iam_user_policy" "lb_ro_quali" {
+  name   = "eks-list-access"
+  user   = aws_iam_user.quali-poc.name
+  policy = data.aws_iam_policy_document.gh-actions-policy.json
+}
+
+data "aws_iam_policy_document" "quali-poc-policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "eks:DescribeCluster",
+      "eks:ListClusters",
+    ]
+    resources = ["*"]
+  }
+}
