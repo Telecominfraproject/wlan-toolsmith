2bba7634ac36a5bd9184e2783ee28f28542f8616
Gleb Boushev <core@4c74356b41.com>
2021-03-03 17:49:13 +0300

    Update aws-cicd-testbed-deployment.yaml.sh

--- helm-values/aws-cicd-testbed-deployment.yaml.sh
+++ helm-values/aws-cicd-testbed-deployment.yaml.sh
@@ -12,14 +12,10 @@ TESTBED_NUMBER=$1
 if [ -z "$2" ];
 then
   # using todays date
-  TODAY=0.0.1-SNAPSHOT-$(date -d "yesterday" +"%Y-%m-%d")
-elif [ "$2" = "latest" ]
-then
-  # using latest tag
-  TODAY=0.0.1-SNAPSHOT
+  TODAY=1.0.0-SNAPSHOT-$(date -d "yesterday" +"%Y-%m-%d")
 else
   # using provided tag
-  TODAY=0.0.1-SNAPSHOT-$2
+  TODAY=$2
 fi
 
 
