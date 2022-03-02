0711c4a119e73f459bfc70369457ab840a94d53a
Johann Hoffmann <johann.hoffmann@mailbox.org>
2022-03-02 16:51:50 +0100

    Add required variables
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>

--- terraform/wifi-289708231103/openwifi-virtual-ap/terraform.tfvars
+++ terraform/wifi-289708231103/openwifi-virtual-ap/terraform.tfvars
@@ -1,1 +1,7 @@
 aws_region = "us-east-1"
+
+env = "main"
+
+project = "wlan"
+
+org = "tip"
--- terraform/wifi-289708231103/openwifi-virtual-ap/variables.tf
+++ terraform/wifi-289708231103/openwifi-virtual-ap/variables.tf
@@ -2,3 +2,17 @@ variable "aws_region" {
   description = "AWS region"
   type        = string
 }
+
+variable "env" {
+  type = string
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+}
+
+variable "org" {
+  description = "Organization name"
+  type        = string
+}
