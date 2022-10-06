c256631a2828bec3bbe36193f5fc8aa8fab1d4b8
Johann Hoffmann <johann.hoffmann@mailbox.org>
2022-10-06 12:47:18 +0200

    [WIFI-10659] Create alert for new content in coredumps s3 bucket (#217)
    * Add lifecycle config to coredump S3 bucket
    
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>
    
    * Add required resources to create S3 event notification and Lambda function
    
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>
    
    * Add handler argument
    
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>
    
    * Fix Terraform format
    
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>
    
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>

--- terraform/wifi-289708231103/core-dumps-s3/main.tf
+++ terraform/wifi-289708231103/core-dumps-s3/main.tf
@@ -12,6 +12,13 @@ terraform {
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
+
+  required_providers {
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 0.5"
+    }
+  }
 }
 
 locals {
@@ -20,9 +27,79 @@ locals {
   }
 }
 
+data "sops_file" "secrets" {
+  source_file = "secrets.enc.json"
+}
+
 resource "aws_s3_bucket" "openwifi-core-dumps" {
   bucket = "openwifi-core-dumps"
   tags   = local.common_tags
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "openwifi-core-dumps" {
+  bucket = aws_s3_bucket.openwifi-core-dumps.id
+
+  rule {
+    id = "core-dumps-retention"
+    filter {}
+    status = "Enabled"
+
+    expiration {
+      days = 14
+    }
+  }
+}
+
+resource "aws_s3_bucket_notification" "s3_eventnotification_slack" {
+  bucket = aws_s3_bucket.openwifi-core-dumps.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s3_eventnotification_slack.arn
+    events              = ["s3:ObjectCreated:Put"]
+  }
+
+  depends_on = [aws_lambda_permission.s3_eventnotification_slack]
+}
+
+resource "aws_iam_role" "s3_eventnotification_slack" {
+  name = "s3_eventnotification_slack"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_permission" "s3_eventnotification_slack" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_eventnotification_slack.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.openwifi-core-dumps.arn
+}
+
+resource "aws_lambda_function" "s3_eventnotification_slack" {
+  filename      = "s3_eventnotification_slack.zip"
+  function_name = "s3_eventnotification_slack"
+  handler       = "s3_eventnotification_slack.lambda_handler"
+  role          = aws_iam_role.s3_eventnotification_slack.arn
+  runtime       = "python3.9"
+
+  environment {
+    variables = {
+      SLACK_WEBHOOK_URL = data.sops_file.secrets.data["slack_webhook_url"]
+    }
+  }
 }
 
 resource "aws_s3_bucket_acl" "openwifi-core-dumps" {
