feb8e97d10e0a7e507660fca546dabdf6be616e4
Johann Hoffmann <johann.hoffmann@mailbox.org>
2022-11-07 12:30:01 +0100

    Fix name tag
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>

--- terraform/wifi-289708231103/allure-reports-s3/main.tf
+++ terraform/wifi-289708231103/allure-reports-s3/main.tf
@@ -29,14 +29,15 @@ data "terraform_remote_state" "route_53" {
 locals {
   common_tags = {
     "ManagedBy" = "terraform"
-    "Name"      = aws_s3_bucket.bucket.bucket
   }
 }
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "openwifi-allure-reports"
   acl    = "public-read"
-  tags   = local.common_tags
+  tags = merge({
+    "Name" : "openwifi-allure-reports"
+  }, local.common_tags)
 
   website {
     index_document = "index.html"
