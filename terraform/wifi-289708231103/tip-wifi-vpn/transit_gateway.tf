08e5db2822aea1c5c6fda6899c24456554c61101
Johann Hoffmann <johann.hoffmann@mailbox.org>
2022-12-29 13:39:23 +0100

    [WIFI-11931] Update Terraform code to reflect changes to AWS VPN tunnels (#234)
    * Update TF code to match infrastructure state
    
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>
    
    * Add tunnel log options to ignore_lifecycle_changes
    
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>
    
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>

--- terraform/wifi-289708231103/tip-wifi-vpn/transit_gateway.tf
+++ terraform/wifi-289708231103/tip-wifi-vpn/transit_gateway.tf
@@ -2,8 +2,8 @@ module "tgw_main" {
   source  = "terraform-aws-modules/transit-gateway/aws"
   version = "~> 2.0"
 
-  name        = "tip-wifi-nrg"
-  description = "tip-wifi-nrg"
+  name        = "tip-wifi-fre"
+  description = "tip-wifi-fre"
   share_tgw   = false
   vpc_attachments = {
     cicd = {
@@ -27,6 +27,6 @@ resource "aws_route" "private" {
 
 resource "aws_ec2_transit_gateway_route" "vpn" {
   destination_cidr_block         = data.sops_file.secrets.data["vpn_endpoint_cidr"]
-  transit_gateway_attachment_id  = aws_vpn_connection.tunnel_tip_wifi_nrg.transit_gateway_attachment_id
+  transit_gateway_attachment_id  = aws_vpn_connection.tunnel_tip_wifi_fre.transit_gateway_attachment_id
   transit_gateway_route_table_id = module.tgw_main.ec2_transit_gateway_association_default_route_table_id
 }
--- terraform/wifi-289708231103/tip-wifi-vpn/vpn.tf
+++ terraform/wifi-289708231103/tip-wifi-vpn/vpn.tf
@@ -1,16 +1,36 @@
-resource "aws_customer_gateway" "tunnel_tip_wifi_nrg" {
+resource "aws_customer_gateway" "tunnel_tip_wifi_fre" {
   bgp_asn    = 65000
   ip_address = data.sops_file.secrets.data["nrg_vpn_endpoint_ip"]
   type       = "ipsec.1"
-  tags       = merge({ Name = "tip-wifi-fre" }, local.common_tags)
+  tags       = merge({ Name = "tip-wifi-fre-133" }, local.common_tags)
 }
 
-resource "aws_vpn_connection" "tunnel_tip_wifi_nrg" {
-  customer_gateway_id = aws_customer_gateway.tunnel_tip_wifi_nrg.id
+resource "aws_customer_gateway" "tunnel_perfecto" {
+  bgp_asn    = 65000
+  ip_address = data.sops_file.secrets.data["perfecto_ip"]
+  type       = "ipsec.1"
+  tags       = merge({ Name = "tunnel-perfecto" }, local.common_tags)
+}
+
+resource "aws_vpn_connection" "tunnel_tip_wifi_fre" {
+  customer_gateway_id = aws_customer_gateway.tunnel_tip_wifi_fre.id
   transit_gateway_id  = module.tgw_main.ec2_transit_gateway_id
   type                = "ipsec.1"
   static_routes_only  = true
   tags                = merge({ Name = "tip-wifi-fre" }, local.common_tags)
+
+  tunnel1_log_options {
+    cloudwatch_log_options {
+      log_enabled   = true
+      log_group_arn = aws_cloudwatch_log_group.vpn_logs.arn
+    }
+  }
+  tunnel2_log_options {
+    cloudwatch_log_options {
+      log_enabled   = true
+      log_group_arn = aws_cloudwatch_log_group.vpn_logs.arn
+    }
+  }
 
   lifecycle {
     ignore_changes = [
@@ -22,13 +42,58 @@ resource "aws_vpn_connection" "tunnel_tip_wifi_nrg" {
       tunnel1_phase2_encryption_algorithms,
       tunnel1_phase2_integrity_algorithms,
       tunnel1_startup_action,
+      tunnel1_log_options,
       tunnel2_ike_versions,
       tunnel2_phase1_dh_group_numbers,
       tunnel2_phase1_encryption_algorithms,
       tunnel2_phase1_integrity_algorithms,
       tunnel2_phase2_dh_group_numbers,
       tunnel2_phase2_encryption_algorithms,
-      tunnel2_phase2_integrity_algorithms
+      tunnel2_phase2_integrity_algorithms,
+      tunnel2_log_options
+    ]
+  }
+}
+
+resource "aws_vpn_connection" "tunnel-perfecto" {
+  customer_gateway_id = aws_customer_gateway.tunnel_perfecto.id
+  transit_gateway_id  = module.tgw_main.ec2_transit_gateway_id
+  type                = "ipsec.1"
+  static_routes_only  = true
+  tags                = merge({ Name = "tunnel-perfecto" }, local.common_tags)
+
+  tunnel1_log_options {
+    cloudwatch_log_options {
+      log_enabled   = true
+      log_group_arn = aws_cloudwatch_log_group.vpn_logs.arn
+    }
+  }
+  tunnel2_log_options {
+    cloudwatch_log_options {
+      log_enabled   = true
+      log_group_arn = aws_cloudwatch_log_group.vpn_logs.arn
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tunnel1_ike_versions,
+      tunnel1_phase1_dh_group_numbers,
+      tunnel1_phase1_encryption_algorithms,
+      tunnel1_phase1_integrity_algorithms,
+      tunnel1_phase2_dh_group_numbers,
+      tunnel1_phase2_encryption_algorithms,
+      tunnel1_phase2_integrity_algorithms,
+      tunnel1_startup_action,
+      tunnel1_log_options,
+      tunnel2_ike_versions,
+      tunnel2_phase1_dh_group_numbers,
+      tunnel2_phase1_encryption_algorithms,
+      tunnel2_phase1_integrity_algorithms,
+      tunnel2_phase2_dh_group_numbers,
+      tunnel2_phase2_encryption_algorithms,
+      tunnel2_phase2_integrity_algorithms,
+      tunnel2_log_options
     ]
   }
 }
